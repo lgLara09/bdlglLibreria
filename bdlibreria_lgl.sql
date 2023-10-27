fbb89db14abdf60997151c76e9f5b3dcaa761bd8
Lizbeth Garcia <143548080+lgLara09@users.noreply.github.com>
2023-10-27 11:39:44 -0600

    Add files via upload

--- bdlibreria_lgl.sql
+++ bdlibreria_lgl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2023 at 07:28 PM
+-- Generation Time: Oct 27, 2023 at 07:38 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,22 +24,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Table structure for table `inicio_sesion`
 --
 
-CREATE TABLE `login` (
+CREATE TABLE `inicio_sesion` (
   `id` int(9) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `nombre` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `contrasena` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `login`
+-- Dumping data for table `inicio_sesion`
 --
 
-INSERT INTO `login` (`id`, `name`, `email`, `username`, `password`) VALUES
+INSERT INTO `inicio_sesion` (`id`, `nombre`, `correo`, `usuario`, `contrasena`) VALUES
 (3, 'nairobi keren', 'nai@gmail.com', 'nai', '202cb962ac59075b964b07152d234b70'),
 (4, 'Beth', 'lizbethgala02@gmail.com', 'liz', 'd9b1d7db4cd6e70935368a1efb10e377'),
 (8, 'Valeria', 'vl2005@gmail.com', 'Val1507', 'cb906433e1289aa1c251dc5057746a88'),
@@ -75,26 +75,6 @@ CREATE TABLE `renta` (
 INSERT INTO `renta` (`id`, `id_renta`, `id_libro`, `id_usuario`, `id_empleado`, `num_factura`, `num_telefono`, `direccion`, `tiempo_renta`, `pago`, `num_cuenta`, `tipo_tarjeta`) VALUES
 (7, 1, 1, 1, 1, 2671, 1111111111, 'hidalgo del parral', '4 meses', 350.00, 39424, 'debito'),
 (8, 23, 50, 56, 73, 26374, 6567849365, 'hidalgo del parral', '3 meses', 350.00, 37439, 'credito');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `login`
---
-ALTER TABLE `login`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
